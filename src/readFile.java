8881ad8e0b852498bab96da9d9b6a97df79ac44b
Matthew <Matthewrcwork@gmail.com>
2019-02-16 13:45:30 -0500

    file reader

---
+++ src/readFile.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class readFile {
+    public static void main(String[] args){
+        String filename = args[0];
+        System.out.println(filename);
+        //File file = new File("test.txt");
+        //for(String fileNames : file.list()) System.out.println(fileNames);
+        File file = new File(filename);
+        charbychar(file);
+    }
+
+    private static void charbychar(File file) {
+        try {
+            FileInputStream in = new FileInputStream(file);
+            char current;
+            while (in.available() > 0) {
+                current = (char) in.read();
+                //do something with the character
+            }
+        }
+        catch (IOException error){
+            error.printStackTrace();
+        }
+    }
+}
