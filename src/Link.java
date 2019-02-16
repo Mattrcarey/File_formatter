81edcb013158365e6dad9a97a1989744868f91b1
Matthew <Matthewrcwork@gmail.com>
2019-02-16 16:54:08 -0500

    added apache poi library to the program

--- src/Link.java
+++ src/Link.java
@@ -3,7 +3,8 @@ public class Link extends FormattedString {
     @Override
     public void print() {
         super.print();
-        System.out.print("\033[0;1m" + super.string);
+        //System.out.print("\033[0;1m" + super.string);
+        System.out.println("\u001B34;1m" + super.string);
     }
 
 }
--- src/Quote.java
+++ src/Quote.java
@@ -1,2 +1,7 @@
 public class Quote extends FormattedString {
+
+    @Override
+    public void print() {
+        System.out.println("\033[3m" + super.string);
+    }
 }
