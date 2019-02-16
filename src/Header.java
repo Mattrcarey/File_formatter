f82c12e7b5c58bc0654a12a9f68847b219a30395
EstrellaM25 <estrella.carcamo@outlook.com>
2019-02-16 17:59:39 -0500

    implemented Header and Title classes

--- src/Header.java
+++ src/Header.java
@@ -1,3 +1,21 @@
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
 public class Header extends FormattedString {
+
+    Header(String string, XWPFParagraph paragraph){
+        super(true, false, false, 12, string, paragraph);
+    }
+
+    @Override
+    public void print(){
+        XWPFRun headerRun = paragraph.createRun();
+        headerRun.setBold(this.b);
+        headerRun.setItalic(this.i);
+        headerRun.setFontSize(this.size);
+        headerRun.setText(this.string);
+        headerRun.addBreak();
+    }
 
 }
--- src/Title.java
+++ src/Title.java
@@ -1,2 +1,27 @@
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.apache.poi.xwpf.usermodel.VerticalAlign;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+import java.io.FileOutputStream;
+
+
 public class Title extends FormattedString {
+
+    Title(String string, XWPFParagraph paragraph){
+        super(true, false, false, 18, string, paragraph);
+    }
+
+    @Override
+    public void print(){
+        XWPFRun titleRun = paragraph.createRun();
+        titleRun.setBold(this.b);
+        titleRun.setItalic(this.i);
+        titleRun.setFontSize(this.size);
+        titleRun.setText(this.string);
+        titleRun.addBreak();
+    }
 }
