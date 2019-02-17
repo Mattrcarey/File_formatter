ae2a84946c610ed673e6f08d467b9f9323b34cc6
Matthew <Matthewrcwork@gmail.com>
2019-02-16 19:33:46 -0500

    Working on main/ formatter

--- src/Formatter.java
+++ src/Formatter.java
@@ -1,35 +1,34 @@
-import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class Formatter {
     private boolean title = false;
     private XWPFDocument doc = new XWPFDocument();
 
-    public void readfile(File file){
-        String filedata;
-        char[] filechars;
-        try{
-            XWPFDocument doc = new XWPFDocument(new FileInputStream(file));
-            XWPFWordExtractor extractor = new XWPFWordExtractor(doc);
-            filedata = extractor.getText();
-            filechars = filedata.toCharArray();
-            int length = filechars.length;
+    public void readfile(File file) throws IOException {
+        XWPFDocument doc = new XWPFDocument(new FileInputStream(file));
+        doc.getParagraphs();
 
-        }catch (Exception e){
-            e.printStackTrace();
-        }
 
-    }
-
-    public void buildfile(String[] chars, int length){
-        for(int i = 0 ; i<length; i++){
-
-        }
+//        String filedata;
+//        char[] filechars;
+//        try{
+//            XWPFDocument doc = new XWPFDocument(new FileInputStream(file));
+//            XWPFWordExtractor extractor = new XWPFWordExtractor(doc);
+//            filedata = extractor.getText();
+//            filechars = filedata.toCharArray();
+//            int length = filechars.length;
+//            buildfile(filechars,length);
+//
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
 
     }
+
 
     //private static void charbychar(File file) {
     //    try {
